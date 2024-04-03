18c2aa6a5c3df6c5e2b5cb301369db84119f3d47
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2024-04-03 21:37:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Header from './Header';
+// import Footer from './Footer';
+import UpdateEmployee from './UpdateEmployee';
+import EmployeesList from './EmployeesList';
+import AddEmployee from './AddEmployee';
+import Contact from './Contact/Contact';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+function App(){
+
+  return(<>
+  <BrowserRouter>
+  
+  <Header/>
+
+  <Routes>
+  <Route path='/contact' element={<Contact/>}/>
+  <Route path='/' element={<EmployeesList/>}/>
+  <Route path='/employee/add' element={<AddEmployee/>}/>
+  <Route path='/employees/:empID/update' element={<UpdateEmployee/>}/>
+  </Routes>
+  {/* <Footer/> */}
+  </BrowserRouter>
+  
+  
+  </>)
+}
+export default App;
